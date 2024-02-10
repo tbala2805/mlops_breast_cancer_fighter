ded6e318502c172697d4e2af0c550f85a857c717
Rushikesh Naik <rushikeshnaik@Rushikesh-m2.local>
2024-02-10 17:44:45 +0530

    adding model saving script

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.training import initializing_model, fitting_model
+from src.training import initializing_model, fitting_model, save_model
 from src.preprocessing import preprocess
 from src.eda import perform_eda
 import pandas as pd
@@ -16,6 +16,7 @@ config = configprsr.read('config.ini')
 path = configprsr['DEFAULT']['path']
 eda_report_path = configprsr['DEFAULT']['eda_report_path']
 model_report_path = configprsr['DEFAULT']['model_report_path']
+model_path = "./models"
 cols_to_drop = configprsr['PREPROCESSING']['cols_to_drop'].split(',')
 target_col = configprsr['PREPROCESSING']['target_col']
 target_classes = configprsr['TARGET_CLASSES']['target_classes']
@@ -43,5 +44,9 @@ model = initializing_model()
 log.info("Model is initialized")
 #
 log.info("Fitting a model")
-fitting_model(model, X, y, model_report_path=model_report_path, target_classes=target_classes)
+model = fitting_model(model, X, y, model_report_path=model_report_path, target_classes=target_classes)
 log.info("Training is completed")
+
+log.info("Saving a model")
+save_model(model, model_path)
+log.info("Model is saved")
